dc10d786eeac1ee51762ef1a4d40819117256fbe
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-07-26 13:58:10 -0400

    Exercise Model in place

--- server/models.py
+++ server/models.py
@@ -2,3 +2,15 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 # Models will go here
+class Exercise(db.Model):
+  __tablename__ = 'exercises'
+
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String, nullable=False, unique=True)
+  category = db.Column(db.String, nullable=False)
+  equipment_needed = db.Column(db.Boolean, nullable=False)
+
+  # Relationships will be here
+
+  def __repr__(self):
+    return f"<Exercise {self.name}>"
